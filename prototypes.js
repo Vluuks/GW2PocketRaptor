4126499536c25708d2546d5580148d2b41e4986d
Vluuks <vluuks@gmail.com>
2017-05-15 15:34:27 +0200

    split classes and dicts to other files

---
+++ prototypes.js
@@ -0,0 +1,72 @@
+/* Contains the "model classes" for the data visualisation. 
+   We need multiple instances of these objects so they come in constructor shape. */
+
+function Character() {
+    this.name = "";
+    this.race = "";
+    this.agonyResist = 0;
+    this.profession = "";
+    this.level = -1;
+    this.equipment = [];
+    this.equipmentRarity = [];
+    this.sunburstDataCache = undefined;
+}
+
+function AgonyResist() {
+    this.total = 0;
+    this.trinkets = 0;
+    this.armor = 0;
+    this.weaponsA = 0;
+    this.weaponsB = 0;
+    this.aquatic = 0;
+}
+
+function Item(id, name, rarity, infusions, type, slot, weaponSlot) {
+    this.id = id;
+    this.name = name;
+    this.rarity = rarity;
+    this.infusions = infusions;
+    this.type = type;
+    this.slot = slot;
+    this.weaponSlot = weaponSlot;
+    this.agonyResist = 0;
+    this.size = 1;
+}
+
+// Actual globals
+
+var account = {
+    name: "",
+    apiKey: "",
+    hoursPlayed: -1,
+    characters: [],
+    characterAmount: -1,
+    fractalLevel: -1,
+    fractalRelics: -1,
+    fractalPristine: -1,
+    characterDictionary: {}
+}
+
+function SunburstBase(){
+    
+    this.name = "Equipment";
+    this.children = [
+
+        {
+            "name": "Armor",
+            "children": []
+        },
+        {
+            "name": "Trinkets",
+            "children": []
+        },
+        {
+            "name": "Weapons",
+            "children": []
+        },
+        {
+            "name": "Aquatic",
+            "children": []
+        },
+    ];   
+}
--- ziekgoeiecodeman.js
+++ ziekgoeiecodeman.js
@@ -20,63 +20,6 @@
 
 /* OBJECTS AND CONSTANTS  ********************************************************************************/
 
-/* Model "class" used to store data about characters. Initializes with default
-values and is further filled in as API requests are completed and data retrieved. */
-function Character() {
-    this.name = "";
-    this.race = "";
-    this.agonyResist = 0;
-    this.profession = "";
-    this.level = -1;
-    this.equipment = [];
-    this.equipmentRarity = [];
-    this.sunburstDataCache = undefined;
-}
-
-/* A global object used to store all the information pertaining to the account
-that is currently using the visualization. */
-var account = {};
-
-/* Dictionary containing color info. */
-var colorDictionary = {
-
-    // Rarities.
-    "Basic": "#f2f2f2",
-    "Fine": "#569fff",
-    "Masterwork": "#6dad1f",
-    "Rare": "#ffc700",
-    "Exotic": "#ff8800",
-    "Ascended": "#dd1a7f",
-    "Legendary": "#8119d1",
-
-    // Categories.
-    "Armor": "#75645b",
-    "Weapons": "#4c4441",
-    "Aquatic": "#8e817c",
-    "Trinkets": "#99837b",
-
-    // Professions.
-    "Guardian": "#2a6cd6",
-    "Dragonhunter": "#2a6cd6",
-    "Revenant": "#a85555",
-    "Herald": "#a85555",
-    "Warrior": "#e2ad18",
-    "Berserker": "#e2ad18",
-    "Engineer": "#915a31",
-    "Scrapper": "#915a31",
-    "Thief": "#66605b",
-    "Daredevil": "#66605b",
-    "Ranger": "#99c661",
-    "Druid": "#99c661",
-    "Elementalist": "#ce3b40",
-    "Tempest": "#ce3b40",
-    "Mesmer": "#ce3bc9",
-    "Chronomancer": "#ce3bc9",
-    "Necromancer": "#3a916e",
-    "Reaper": "#3a916e"
-}
-
-
 /* SCRIPT *********************************************************************************************************/
 
 /* Wait until page is ready. */
@@ -106,19 +49,6 @@ function showError(errorMessage) {
 This function is invoked by pressing the button on the webpage. */
 function getUserApi() {
 	
-	// Initialize account variable.
-	account = {
-		name: "",
-		apiKey: "",
-		hoursPlayed: -1,
-		characters: [],
-		characterAmount: -1,
-		fractalLevel: -1,
-		fractalRelics: -1,
-		fractalPristine: -1,
-		characterDictionary: {}
-	}
-
     // Check if svgs were already made, if so, delete.
     if ($("#barchartsvg")) {
         $("#barchartsvg").remove();
@@ -142,7 +72,7 @@ function getUserApi() {
     var apiKey = $("#apiKey").val().trim();
     
     apiKey = "F42B9440-82CB-0D4A-AA45-1594E292B1FB08137C88-69C5-4779-8740-43FA4C501EE0";
-    apiKey = "A2D523A7-B023-554F-898C-A7D631E287B40F27ED03-D8EA-4304-B0B6-E839DA12F709";
+    // apiKey = "A2D523A7-B023-554F-898C-A7D631E287B40F27ED03-D8EA-4304-B0B6-E839DA12F709";
 
     if (apiKey == "" || apiKey == undefined) {
         showError("Please do not omit the field");
@@ -389,21 +319,19 @@ function fetchEquipment() {
 
                             // Store item properties in object and store object in the array of items on the character.
                             if (itemObject.type == ("Armor") || itemObject.type == ("Trinket") || itemObject.type == ("Weapon") || itemObject.type == ("Back")) {
-
-                                var newItemObject = {
-                                    id: itemObject.id,
-                                    name: itemObject.name,
-                                    rarity: itemObject.rarity,
-                                    infusions: infusionsPerPieceDict[itemObject.id], 
-                                    type: itemObject.type,
-                                    slot: itemObject.details["type"],
-                                    weaponSlot: slotInformationDict[itemObject.id],
-                                    agonyResist: 0,
-                                    size: 1
-                                }
-
+                                
+                                var itemObject = new Item(
+                                    itemObject.id,
+                                    itemObject.name,
+                                    itemObject.rarity,
+                                    infusionsPerPieceDict[itemObject.id], 
+                                    itemObject.type,
+                                    itemObject.details["type"],
+                                    slotInformationDict[itemObject.id]
+                                )
+                                
                                 // Push to equipment array. 
-                                account.characterDictionary[character].equipmentRarity.push(newItemObject);
+                                account.characterDictionary[character].equipmentRarity.push(itemObject);
                             }
                         }
 
@@ -425,102 +353,8 @@ revamps of the system, which makes a dictionary necessary to account for all pos
 If no infusions are present the infusionsarray will not exist and the function will return 0. */
 function calculateAgonyResist(equipment, character) {
 
-    // A dictionary containing key value pairs of item ids and agony resist.
-    var infusionDictionary = {
-
-        // Stat infusions, legacy infusions, aura infusions.
-        "75480": 3,
-        "37137": 5,
-        "37138": 5,
-        "37140": 5,
-        "39616": 5,
-        "39617": 5,
-        "39618": 5,
-        "39619": 5,
-        "39620": 5,
-        "39621": 5,
-        "70852": 7,
-        "37123": 7,
-        "37127": 7,
-        "37128": 7,
-        "37129": 7,
-        "37133": 7,
-        "37134": 7,
-        "78028": 9,
-        "78052": 9,
-        "78012": 9,
-        "78016": 9,
-        "78030": 9,
-        "78031": 9,
-        "78054": 9,
-        "78057": 9,
-        "78079": 9,
-        "78086": 9,
-        "78090": 9,
-        "78097": 9,
-        "79639": 9,
-        "79653": 9,
-        "79661": 9,
-        "79665": 9,
-        "79669": 9,
-        "79674": 9,
-        "77274": 9,
-        "77303": 9,
-        "77310": 9,
-        "77316": 9,
-        "77366": 9,
-        "77394": 9,
-        "79943": 9,
-        "79957": 9,
-        "79959": 9,
-        "79978": 9,
-        "79994": 9,
-        "80063": 9,
-        "37125": 9,
-        "37130": 9,
-        "37131": 9,
-        "37132": 9,
-        "37135": 9,
-        "37136": 9,
-        "37131": 9,
-
-        // Regular infusions that can be broken down/crafted.
-        "49424": 1,
-        "49425": 2,
-        "49426": 3,
-        "49427": 4,
-        "49428": 5,
-        "49429": 6,
-        "49430": 7,
-        "49431": 8,
-        "49432": 9,
-        "49433": 10,
-        "49434": 11,
-        "49435": 12,
-        "49436": 13,
-        "49437": 14,
-        "49438": 15,
-        "49439": 16,
-        "49440": 17,
-        "49441": 18,
-        "49442": 19,
-        "49443": 20,
-
-        // Nonsense values map to 0 for safety.
-        undefined: 0,
-        "undefined": 0,
-        NaN: 0
-    };
-
-    // Counters to track different sources of AR.
-    var agonyResist = {
-        total: 0,
-        trinkets: 0,
-        armor: 0,
-        weaponsA: 0,
-        weaponsB: 0,
-        aquatic: 0
-    };
+    // Instantiate new agonyresist object.
+    var agonyResist = new AgonyResist();
 
     // Iterate over all the items.
     for (item in equipment) {
@@ -907,35 +741,15 @@ function transformDataForSunburst(character) {
 
     // Set loading spinner.
     $('#sunburstloading').show();
-
+    var sunburstObject = new SunburstBase();
+    
     // Check if the data has been cached to avoid recreating the object for nothing.
     if (account.characterDictionary[character].sunburstDataCache == undefined) {
+        
+        console.log("new character");
 
         // Get the equipment array containing objects form the character dictionary.
         var equipment = account.characterDictionary[character].equipmentRarity;
-        var sunburstObject = {
-
-            "name": "Equipment",
-            "children": [
-
-                {
-                    "name": "Armor",
-                    "children": []
-                },
-                {
-                    "name": "Trinkets",
-                    "children": []
-                },
-                {
-                    "name": "Weapons",
-                    "children": []
-                },
-                {
-                    "name": "Aquatic",
-                    "children": []
-                },
-            ]
-        };
 
         // Loop over the equipment pieces and construct data accordingly.
         for (var piece in equipment) {
@@ -977,6 +791,8 @@ function transformDataForSunburst(character) {
 information about all the gear that a character has on them, and the rarity and name of these
 items. */
 function makeSunburst(data) {
+    
+    console.log(data);
 
     // Hide the information message.
     $('#sunburstwait').hide();
