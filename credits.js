744df460233b4c5f35684ffc6c0d23d41a0d1b3f
Vluuks <vluuks@gmail.com>
2017-05-16 20:47:10 +0200

    added todos

--- credits.js
+++ credits.js
@@ -15,5 +15,24 @@
 	> https://www.w3schools.com/bootstrap/tryit.asp?filename=trybs_tabs_dynamic&stacked=h
 	> https://jsfiddle.net/8sh069ns/
     > http://plnkr.co/edit/tct95toQ2IjyUkQJQPB9?p=preview  
+    
+    
+    
+    
+    TODO:
+    > make pie chart of class distribution
+    > also race
+    > also gender?? might be kinda pointless but well
+    
+    > better tooltip with more item info or rather no tooltip but a separate
+    area that shows the item details? might be easier to implement + more
+    pleasant on the eye
+    
+    div > show elements when hovering and then hide again when mouseout
+    
+    item name
+    item icon
+    + all other data that is retrieved from the API could also be included
+    rarity in appropriate color
 	
 */
