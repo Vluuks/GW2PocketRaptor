3b1f2e3396bb0c2a75dfd7e4f985255943900635
Vluuks <vluuks@gmail.com>
2019-09-08 14:06:04 +0200

    adding to array

--- Scripts/characterListHandler.js
+++ Scripts/characterListHandler.js
@@ -1,7 +1,15 @@
+let app;
+
 function initialize() {
-    var app = new Vue({
+    app = new Vue({
         el: '#charoverview-container',
         data: {
+            characters: []
         }
     });
+}
+
+function addCharacter() {
+    Vue.set(app.characters, app.characters.length, "test")
+
 }
