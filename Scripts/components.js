d98367a4bbadf4105a3dc7a0fe9329d99d00062b
Vluuks <vluuks@gmail.com>
2019-09-08 16:00:47 +0200

    component file added

---
+++ Scripts/components.js
@@ -0,0 +1,29 @@
+
+Vue.component('character', {
+    props: {
+        character: {
+            type: Object,
+            required: true
+        }
+    },
+    name: "character",
+    template: '#character-template',
+    computed: {
+        professionColor() {
+            return colorDictionary[this.character.profession];
+        },
+        professionName() {
+            if(this.character.eliteSpec != "" && this.character.eliteSpec != undefined) {
+                return this.character.eliteSpec;
+            }
+            return this.character.profession;
+        },
+        professionImage() {
+            if(this.character.eliteSpec != "" && this.character.eliteSpec != undefined) {
+                return 'Static/Professions/' + professionImageDictionary[this.character.eliteSpec] + '.png';
+            }
+            return 'Static/Professions/' + professionImageDictionary[this.character.profession] + '.png';
+        }
+    }
+});
+
