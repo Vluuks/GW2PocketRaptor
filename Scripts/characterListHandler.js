7f9f6e5738a601e387c5f047fb3125ff5d238720
Vluuks <vluuks@gmail.com>
2019-09-08 18:46:19 +0200

    basic search functionality

--- Scripts/characterListHandler.js
+++ Scripts/characterListHandler.js
@@ -4,7 +4,15 @@ function initialize() {
     app = new Vue({
         el: '#charoverview-container',
         data: {
-            characters: []
+            characters: [],
+            search: "",
+            professionCriteria: [],
+            sort: "",
+        },
+        computed: {
+            filteredCharacters() {
+                return filter(this.characters, this.search, "all");
+            }
         }
     });
 }
@@ -14,5 +22,26 @@ function addCharacter(character) {
     console.log(character)
     let char = character; // if i push character directly, it does not work?
     Vue.set(app.characters, app.characters.length, char)
+
+}
+
+function matchStringNoCase(source, comparison){
+    return source.toUpperCase().match(comparison.toUpperCase());
+}
+
+/*
+    Filters items in the collection based on the selected category and search field input. Compares against
+    tags, description and item name. 
+*/
+function filter(collection, searchTerm, category) {
+    
+    return collection.filter((item) => {
+
+        // Apply category filter
+        if(category != "all")
+            return category == item.category && (matchStringNoCase(item.name, searchTerm))
+
+        return matchStringNoCase(item.name, searchTerm) ;
+    });
 
 }
