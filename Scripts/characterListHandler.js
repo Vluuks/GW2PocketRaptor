eda47ef64bbde42d1b7d1a542ce0a8c66aa5d911
Vluuks <vluuks@gmail.com>
2019-09-11 17:36:20 +0200

    duplicate method

--- Scripts/characterListHandler.js
+++ Scripts/characterListHandler.js
@@ -28,31 +28,6 @@ function addCharacter(character) {
 }
 
 
-function drawGearProgress2(percentage, character) {
-
-    
-    var svg = d3.select("#"+character)
-    let percentages = [15, 60, 25];
-    let colors = ["#8119d1", "#dd1a7f", "#d3d3d3"]
-
-    var g = svg.append("g")
-        .attr("width", 250)
-        .attr("height", 20)
-
-    let offset = 0;
-    percentages.forEach(function(prct, i){
-        
-        g.append("rect")
-            .attr("width", prct*2)
-            .attr("height", 20)
-            .style("fill", colors[i])
-            .attr("x", offset)      
-            
-        offset+=(prct*2)
-    })
-}
-
-
 function matchStringNoCase(source, comparison){
     return source.toUpperCase().match(comparison.toUpperCase());
 }
